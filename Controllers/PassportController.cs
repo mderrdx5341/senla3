0b7dff5034c12b4da44ab8815414617f895ad769
Roman Verkeev <mderrdx5341@gmail.com>
2021-10-08 17:13:04 +0300

    added comment, inheritance

--- Controllers/PassportController.cs
+++ Controllers/PassportController.cs
@@ -4,21 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
 using Passports.Models;
 using Passports.Services;
 
 namespace Passports.Controllers
 {
+    /// <summary>
+    /// Контроллер для работы с паспортами
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
-    public class PassportController
+    public class PassportController : ControllerBase
     {
         private readonly PassportService _passportServie;
         public PassportController(PassportService passportService)
         {
             _passportServie = passportService;
         }
+        /// <summary>
+        /// Получение списка паспортов
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult<List<Passport>> List()
         {
