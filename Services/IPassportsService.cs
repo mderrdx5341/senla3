6a7727cb3972d7e5d9dcabcd69ce9f6a984a4c91
Roman Verkeev <mderrdx5341@gmail.com>
2021-10-09 11:30:34 +0300

    added interface

---
+++ Services/IPassportsService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Passports.Models;
+
+namespace Passports.Services
+{
+    /// <summary>
+    /// Интерфейс сервиса для работы с паспортами
+    /// </summary>
+    internal interface IPassportsService
+    {
+        /// <summary>
+        /// Список всех паспортов
+        /// </summary>
+        /// <returns></returns>
+        public List<Passport> GetPassports();
+    }
+}
