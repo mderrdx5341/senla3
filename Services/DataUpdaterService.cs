636a23ae5c40892e7056b45dffc9a4db45c1412d
Roman Verkeev <mderrdx5341@gmail.com>
2021-10-11 11:25:57 +0300

    renamed method

--- Services/DataUpdaterService.cs
+++ Services/DataUpdaterService.cs
@@ -48,7 +48,7 @@ namespace Passports.Services
 
         private void StartUpdate(object o)
         {
-            if (isUpdate())
+            if (isNeedUpdate())
             {
                 _timer.Change(Timeout.Infinite, Timeout.Infinite);
                 _nextUpdate = _nextUpdate.AddDays(1);
@@ -57,7 +57,7 @@ namespace Passports.Services
             }
         }
 
-        private bool isUpdate()
+        private bool isNeedUpdate()
         {
             TimeSpan ts = _nextUpdate.Subtract(DateTime.Now);
             return (ts.CompareTo(TimeSpan.Zero) < 0);
